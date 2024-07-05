83276820c586fc4a977bb69f87f1bcb05ea10928
Ankita Sinha Ray <109851529+ankitasray@users.noreply.github.com>
2024-07-06 01:20:29 +0530

    Create problem1.cpp

---
+++ SemiFinal_Round/problem1.cpp
@@ -0,0 +1,49 @@
+Question1 : Sheldon is alone in the apartment as the other boys have left for Antarctica for a research project. He is feeling lonely and is not doing well. Penny notices it and asks Sheldon if he is willing to have a break and go to the amusement park. Sheldon agrees to it and is overjoyed.
+As they enter the amusement park, they see there are multiple rides and games on their left and right-hand side. The price for each of them is placed next to them. They also notice that some of the games are complimentary. Sheldon has M amount of money with him. Sheldon looks at Penny and smirks and they have the below conversation.
+Sheldon: If you can show the basic elements of intelligence, you can continue using our free Wi-Fi.
+
+Penny: What?
+
+Sheldon: You see there are rides and games on both sides. They all have prices placed next to them and are in a sequential manner. Do you see it?
+
+Penny: Yes, Sheldon. What's the point?
+
+Sheldon: I have M amount of money with me. If you can give me the maximum number of rides and games we can go to, you can continue using our Wi-Fi which you are already using. Oh, but it is not going to be so easy. Since the rides are in a sequential manner, you have to follow that on both sides. 
+
+Penny: I am confused.
+
+Sheldon: Of course, you are! I would have been amused if you would have got it. Here is an example to help you.
+
+
+Example:
+
+Amount of money with Sheldon, M = 20
+
+Price of Rides on the left side = { 5, 3, 6, 8, 10, 20} 
+
+Price of Rides on the right side = { 4, 4, 4, 6, 7, 10, 4, 8 }
+
+
+Pick the first three rides from the right side, Price = 4 + 4 + 4 = 12
+
+Pick the first two rides from the left side, Price = 5 + 3 = 8
+
+
+Total money = 12 + 8 = 20
+
+
+This is the money we have. The maximum number of rides we can take is 5.
+
+Do you get it now, Penny?
+
+
+Penny: (*Still not clear) YES! Do you think only you are a genius?
+
+Sheldon: Great. Prove me wrong.
+
+
+Penny does not like being humiliated by Sheldon but is still confused about what Sheldon said. To top it off, she is bad at calculations. She wants to get the correct answer so she can keep enjoying the free Wi-Fi and get back at Sheldon. Can you help Penny get the right answer?
+
+Solution in C++ :
+
+  
